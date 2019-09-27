0ee863b7e7c1a9d0749c7a9bcd8f5192dbb8c102
NMP300 <t.nmp300@gmail.com>
2019-09-27 16:47:54 +0900

    add new

--- app.rb
+++ app.rb
@@ -2,12 +2,23 @@
 
 require "sinatra"
 require "sinatra/reloader"
+require "securerandom"
+
+FILE_ID = SecureRandom.uuid
 
 get "/" do
+  @memo_list = Dir.glob("memos/*")
   erb :home
 end
 
+get "/new" do
+  erb :new
+end
+
 post "/new" do
+  @memo = params[:memo]
+  @file = File.open("memos/#{FILE_ID}.txt", "w") { |f| f.puts "#{@memo}" }
+  redirect "/"
   erb :new
 end
 
@@ -15,6 +26,8 @@ get "/edit" do
   erb :edit
 end
 
-get "/memos/" do
-  "show memos "
+get "/memo" do
+  "show memo "
 end
+
+
