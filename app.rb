ad1f4ff534b74b1cc4dcf628b53e8b3d755b8b17
NMP300 <t.nmp300@gmail.com>
2019-09-30 18:40:09 +0900

    radical change

--- app.rb
+++ app.rb
@@ -5,36 +5,41 @@ require "sinatra/reloader"
 require "securerandom"
 require "json"
 
-FILE_ID = SecureRandom.uuid
-
-get "/" do
-  @files = Dir.glob("memos/*")
-
-  def show_title(file)
-    memo = File.open("#{file}").read
-    hash = JSON.parse(memo)
-    title = "<a href=memos/#{hash["id"]}>#{hash["title"]}</a>"
-  end
-
-  @title_list = @files.map { |f| show_title(f) }.join("<br>")
-  erb :home
-end
-
-get "/new" do
-  erb :new
-end
-
 post "/new" do
-  @memo_id = FILE_ID
-  @memo = { id: @memo_id, title: params[:title], body: params[:body] }
+  @id = SecureRandom.uuid
+  @memo = { id: @id, title: params[:title], body: params[:body] }
   File.open("memos/#{@memo[:id]}.json", "w") { |f| f.puts JSON.pretty_generate(@memo) }
   erb :new
 end
 
-get "/edit" do
-  erb :edit
-end
+# FILE_ID = SecureRandom.uuid
 
-get "/memo" do
-  "show memo "
-end
+# get "/" do
+#   @files = Dir.glob("memos/*")
+
+#   def show_title(file)
+#     memo = File.open("#{file}").read
+#     @hash = JSON.parse(memo)
+#     @title = "<a href=show/#{@hash["id"]}>#{@hash["title"]}</a>"
+#   end
+
+#   @title_list = @files.map { |f| show_title(f) }.join("<br>")
+#   erb :top
+# end
+
+# get "/new" do
+#   erb :new
+# end
+
+# post "/new" do
+#   @memo_id = FILE_ID
+#   @memo = { id: @memo_id, title: params[:title], body: params[:body] }
+#   File.open("memos/#{@memo[:id]}.json", "w") { |f| f.puts JSON.pretty_generate(@memo) }
+#   erb :new
+# end
+
+
+
+# get "/edit" do
+#   erb :edit
+# end
