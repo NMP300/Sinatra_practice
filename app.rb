d65ad8ecbd8d86251e2a40754d183ac80446cd88
NMP300 <t.nmp300@gmail.com>
2019-09-30 13:17:29 +0900

    formated json_file

--- app.rb
+++ app.rb
@@ -8,9 +8,18 @@ require "json"
 FILE_ID = SecureRandom.uuid
 
 get "/" do
-  memo = open("memos/#{@name[:id]}").read
-  hash = JSON.parse(memo)
-  title = "<a href=memo/#{@id}>#{hash["title"]}</a>"
+
+  @files = Dir.glob("memos/*")
+
+  def show_title(file)
+    memo = File.open("#{file}").read
+    hash = JSON.parse(memo)
+    title = "<a href=memo/#{hash["id"]}>#{hash["title"]}</a>"
+  end
+
+  @title_list = @files.map { |f| show_title(f) }
+
+
   # memo = open("memos/#{@name}").read
   # hash = JSON.parse(memo)
   # @id  = hash["id"]
@@ -25,8 +34,8 @@ end
 
 post "/new" do
   @memo_id = FILE_ID
-  @memo = { id: "#{@memo_id}", title: params[:title], body: params[:body] }
-  File.open("memos/#{@memo[:id]}.json", "w") { |f| f.print "#{@memo.to_json}" }
+  @memo = { id: @memo_id, title: params[:title], body: params[:body] }
+  File.open("memos/#{@memo[:id]}.json", "w") { |f| f.puts JSON.pretty_generate(@memo) }
   erb :new
 end
 
