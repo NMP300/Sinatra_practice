3f66ee5decf2da7fed7e72f1cced42b858ba0637
NMP300 <t.nmp300@gmail.com>
2019-10-03 20:22:10 +0900

    finish refactaring

--- app.rb
+++ app.rb
@@ -5,61 +5,94 @@ require "sinatra/reloader"
 require "securerandom"
 require "json"
 
-enable :method_override
+:method_override
+
+class Memo
+  def initialize
+    @contents = {}
+  end
+
+  def create(hash)
+    File.open("memos/#{hash[:id]}.json", "w") { |f| f.puts JSON.pretty_generate(hash) }
+  end
+
+  def delete(file)
+    File.delete(file)
+  end
+
+  def contents(file)
+    @contents = JSON.parse(File.read(file))
+  end
+
+  def id(file)
+    contents(file)
+    @contents["id"]
+  end
+
+  def title(file)
+    contents(file)
+    @contents["title"]
+  end
+
+  def body(file)
+    contents(file)
+    @contents["body"]
+  end
+
+  def title_list
+    file_list = Dir.glob("memos/*")
+    file_list.map do |file|
+      contents("#{file}")
+      "<a href=memos/#{@contents["id"]}>#{@contents["title"]}</a>"
+    end
+  end
+end
+
+get "/memos" do
+  @title_list = Memo.new.title_list.join("<br>")
+  erb :top
+end
 
 get "/memos/new" do
   erb :new
 end
 
 post "/memos/new" do
-  @id = SecureRandom.uuid
-  @memo = { id: @id, title: params[:title], body: params[:body] }
-  File.open("memos/#{@id}.json", "w") { |f| f.puts JSON.pretty_generate(@memo) }
+  id       = SecureRandom.uuid
+  contents = { id: id, title: params[:title], body: params[:body] }
+  Memo.new.create(contents)
   erb :new
   redirect "/memos"
 end
 
-def memo_detail(memo)
-  @memo = JSON.parse(File.read(memo))
-end
-
-@file_list = Dir.glob("/memos/*")
-
+# 改善点：DRY原則に反している
 get "/memos/:id" do
-  memo_detail("memos/#{params[:id]}.json")
-  @id    = @memo["id"]
-  @title = @memo["title"]
-  @body  = @memo["body"]
-  erb :memos
+  @memo_id    = Memo.new.id("memos/#{params[:id]}.json")
+  @memo_title = Memo.new.title("memos/#{params[:id]}.json")
+  @memo_body  = Memo.new.body("memos/#{params[:id]}.json")
+  erb :memo
 end
 
-get "/memos" do
-  @file_list = Dir.glob("memos/*")
-
-  @title_list = @file_list.map do |file|
-    memo_detail("#{file}")
-    "<a href=memos/#{@memo["id"]}>#{@memo["title"]}</a>"
-  end
-  erb :top
-end
-
-delete "/memos/:id" do
-  @id = params[:id]
-  File.delete("memos/#{@id}.json")
-  redirect "/memos"
-end
-
+# 改善点：DRY原則に反している
 get "/memos/:id/edit" do
-  memo_detail("memos/#{params[:id]}.json")
-  @id    = @memo["id"]
-  @title = @memo["title"]
-  @body  = @memo["body"]
+  @memo_id    = Memo.new.id("memos/#{params[:id]}.json")
+  @memo_title = Memo.new.title("memos/#{params[:id]}.json")
+  @memo_body  = Memo.new.body("memos/#{params[:id]}.json")
   erb :edit
 end
 
 patch "/memos/:id" do
-  @memo = { id: params[:id], title: params[:title], body: params[:body] }
-  File.open("memos/#{params[:id]}.json", "w") { |f| f.puts JSON.pretty_generate(@memo) }
+  new_contents = { id: params[:id], title: params[:title], body: params[:body] }
+  Memo.new.create(new_contents)
   erb :edit
-  redirect "/memos/#{params[:id]}"
+  redirect "/memos"
+end
+
+delete "/memos/:id" do
+  Memo.new.delete("memos/#{params[:id]}.json")
+  redirect "/memos"
+end
+
+get "/practice" do
+  erb :practice
 end
